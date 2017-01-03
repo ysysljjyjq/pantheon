5a8941b25ef5bcde2fb0c6d0cb987a56583f06e7
Greg D. Hill <greghill@cs.stanford.edu>
2017-01-03 09:02:21 +0000

    compare_two_runs.py: fix formatting

--- analyze/compare_two_runs.py
+++ analyze/compare_two_runs.py
@@ -9,11 +9,8 @@ from tabulate import tabulate
 import plot_summary
 from helpers.pantheon_help import check_call
 
-def fmt(number):
-    return '{:.2f}'.format(number)
-
 def get_difference(metric_1, metric_2):
-    return '{:+.1%}'.format(((metric_2 - metric_1)) / metric_1)
+    return '{:+.2%}'.format((metric_2 - metric_1) / metric_1)
 
 
 parser = argparse.ArgumentParser()
@@ -41,6 +38,13 @@ for experiment in experiments:
 exp1_data = plot_summary.PlotSummary(True, False, experiment_dirs[0]).plot_summary()
 exp2_data = plot_summary.PlotSummary(True, False, experiment_dirs[1]).plot_summary()
 
+'''
+print(exp1_data)
+print('\n')
+print(exp2_data)
+print('\n')
+'''
+
 exp_1_schemes = set(exp1_data.keys())
 exp_2_schemes = set(exp2_data.keys())
 common_schemes = exp_1_schemes & exp_2_schemes 
@@ -57,21 +61,22 @@ for scheme in common_schemes:
 
     exp1_throughput_mean = np.mean(exp1_tputs)
     exp2_throughput_mean = np.mean(exp2_tputs)
+
     exp1_throughput_std = np.std(exp1_tputs)
     exp2_throughput_std = np.std(exp2_tputs)
 
-    throughput_lines.append([scheme, exp1_runs, exp2_runs, 'throughput (Mbit/s)', fmt(exp1_throughput_mean), fmt(exp2_throughput_mean), get_difference(exp1_throughput_mean, exp2_throughput_mean),
-            fmt(exp1_throughput_std), fmt(exp2_throughput_std), get_difference(exp1_throughput_std, exp2_throughput_std)])
+    throughput_lines.append([scheme, exp1_runs, exp2_runs, 'throughput (Mbit/s)', exp1_throughput_mean, exp2_throughput_mean, get_difference(exp1_throughput_mean, exp2_throughput_mean),
+            exp1_throughput_std, exp2_throughput_std, get_difference(exp1_throughput_std, exp2_throughput_std)])
 
     exp1_delay_mean = np.mean(exp1_delays)
     exp2_delay_mean = np.mean(exp2_delays)
     exp1_delay_std = np.std(exp1_delays)
     exp2_delay_std = np.std(exp2_delays)
 
-    delay_lines.append([scheme, exp1_runs, exp2_runs, '95th percentile delay (ms)', fmt(exp1_delay_mean), fmt(exp2_delay_mean), get_difference(exp1_delay_mean, exp2_delay_mean),
-            fmt(exp1_delay_std), fmt(exp2_delay_std), get_difference(exp1_delay_std, exp2_delay_std)])
+    delay_lines.append([scheme, exp1_runs, exp2_runs, '95th percentile delay (ms)', exp1_delay_mean, exp2_delay_mean, get_difference(exp1_delay_mean, exp2_delay_mean),
+            exp1_delay_std, exp2_delay_std, get_difference(exp1_delay_std, exp2_delay_std)])
 
 output_headers = ['scheme', 'exp 1 runs', 'exp 2 runs', 'aggregate metric', 'mean 1', 'mean 2', '% difference', 'std dev 1', 'std dev 2', '% difference']
 
 print('Comparison of: %s and %s' % (experiment_dirs[0], experiment_dirs[1]))
-print tabulate(throughput_lines + delay_lines, headers=output_headers)
+print tabulate(throughput_lines + delay_lines, headers=output_headers, floatfmt=".2f", stralign="right")
