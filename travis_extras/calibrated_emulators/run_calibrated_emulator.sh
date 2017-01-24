cd20d702162b2c35565256c5a099a6dabeada09e
Greg D. Hill <greghill@cs.stanford.edu>
2017-01-24 14:43:17 -0800

    update directories

--- travis_extras/calibrated_emulators/run_calibrated_emulator.sh
+++ travis_extras/calibrated_emulators/run_calibrated_emulator.sh
@@ -8,6 +8,6 @@ DOWN_LOSS=$5
 DIR=$6
 SCHEMES=$7
 
-pantheon/test/run.py --uplink-trace $DIR"$BANDWIDTH"mbps.trace --downlink-trace $DIR"$BANDWIDTH"mbps.trace --prepend-mm-cmds "mm-delay $DELAY mm-loss uplink $UP_LOSS mm-loss downlink $DOWN_LOSS" --extra-mm-link-args "--uplink-queue=droptail --uplink-queue-args=packets=$QUEUE" --schemes $SCHEMES --run-times 10
+test/run.py --uplink-trace $DIR"$BANDWIDTH"mbps.trace --downlink-trace $DIR"$BANDWIDTH"mbps.trace --prepend-mm-cmds "mm-delay $DELAY mm-loss uplink $UP_LOSS mm-loss downlink $DOWN_LOSS" --extra-mm-link-args "--uplink-queue=droptail --uplink-queue-args=packets=$QUEUE" --schemes $SCHEMES --run-times 10
 
-pantheon/analyze/compare_two_experiments.py $DIR pantheon/test/ --analyze-schemes $SCHEMES
+analyze/compare_two_experiments.py $DIR test/ --analyze-schemes $SCHEMES
